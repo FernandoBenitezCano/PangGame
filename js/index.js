0bace935e3d94eaa74bd845658e8ab61221376e3
Fer208 <fbenitez-canov01@iesalbarregas.es>
2023-10-31 19:04:29 +0100

    Commented script
    I have commented on the script that hides the message.

--- js/index.js
+++ js/index.js
@@ -2,9 +2,7 @@ let body = document.getElementsByTagName("body")[0];
 let startMsg= document.getElementById("startMsg");
 
 function keyPressed(event) {
-    startMsg.classList.toggle("hide");
-    console.log("Tecla presionada: " + event.key);
-
+    startMsg.classList.toggle("hide");// startMsg it's hide
     // Delete the listener after the first key pressed
     document.removeEventListener("keydown", keyPressed);
 }
