29e8aca32b9b76ea93f51952dc38b004e4023db9
Fer208 <fbenitez-canov01@iesalbarregas.es>
2023-11-01 04:22:15 +0100

    Instructions for the program
    I have created a function called "startGame," which will be called when any key is pressed. Inside this function, there are a series of instructions that are commented out.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,24 @@
 let body = document.getElementsByTagName("body")[0];
+let gameBoardElement= document.getElementById("gameBoard");
 let startMsg= document.getElementById("startMsg");
 
 function keyPressed(event) {
     startMsg.classList.toggle("hide");// startMsg it's hide
     // Delete the listener after the first key pressed
     document.removeEventListener("keydown", keyPressed);
+
+    startGame;
 }
 
 document.addEventListener("keydown", keyPressed);
+
+
+//complete the function startGame
+function startGame(){ 
+    //1 the ball start to fall and the character can move
+    //2 If the ball hits with the character, the charater loses 1 life
+    //2.1 If the character loses three lives, the defeat message will be displayed.
+    // Otherwise, the victory message will be displayed.
+    //3 If the ball hits with the bullet, she will die
+    //4 If the boot hits the ground, walls, or ceiling, it will bounce back.
+}
