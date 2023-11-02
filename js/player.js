6d0df4cd387eb648bf846b548cfc54395fcb148e
javierprogrameitor <javierprogrameitor@gmail.com>
2023-11-02 16:39:54 +0100

    Create player.js
    Movement design

---
+++ js/player.js
@@ -0,0 +1,25 @@
+const player = document.getElementById("player");
+      const playerSpeed = 15;
+
+      // Event listener para detectar las teclas 'A' y 'D'
+      document.addEventListener("keydown", function (event) {
+        if (event.key === "a") {
+          movePlayer(-playerSpeed);
+        } else if (event.key === "d") {
+          movePlayer(playerSpeed);
+        }
+      });
+
+      // Función para mover el jugador y limitar su posición dentro del cuerpo
+      function movePlayer(deltaX) {
+        const playerRect = player.getBoundingClientRect();
+        const bodyRect = document.body.getBoundingClientRect();
+        const newLeft = playerRect.left + deltaX;
+
+        if (
+          newLeft >= bodyRect.left &&
+          newLeft + playerRect.width <= bodyRect.right
+        ) {
+          player.style.left = newLeft + "px";
+        }
+      }
