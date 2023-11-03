c136c4e141a5407eeedd801748fee1f3eb208fa9
javierprogrameitor <javierprogrameitor@gmail.com>
2023-11-03 17:39:42 +0100

    Change in player.js
    Commit to player.js

--- js/player.js
+++ js/player.js
@@ -1,26 +1,21 @@
+// Importa la clase GameObject desde gameObject.js
+import { GameObject } from "./gameObject.js";
+import { setCustomProperty } from "./updateProperties.js";
+// Define la clase player que extiende GameObject
 
-const player = document.getElementById("player");
-      const playerSpeed = 15;
+export class Player extends GameObject {
+  constructor(x, y, width, height) {
+    super(x, y, width, height);
+    this.setElement();
+  }
 
-      // Event listener para detectar las teclas 'A' y 'D'
-      document.addEventListener("keydown", function (event) {
-        if (event.key === "a") {
-          movePlayer(-playerSpeed);
-        } else if (event.key === "d") {
-          movePlayer(playerSpeed);
-        }
-      });
+  setElement() {
+    
+    this.element = document.createElement("div");      
+    this.element.classList.toggle("player");
+    this.element.style.width = this.width + "px";
+    this.element.style.height = this.height + "px";
+    document.body.appendChild(this.element);
+  }
 
-      // Función para mover el jugador y limitar su posición dentro del cuerpo
-      function movePlayer(deltaX) {
-        const playerRect = player.getBoundingClientRect();
-        const bodyRect = document.body.getBoundingClientRect();
-        const newLeft = playerRect.left + deltaX;
-
-        if (
-          newLeft >= bodyRect.left &&
-          newLeft + playerRect.width <= bodyRect.right
-        ) {
-          player.style.left = newLeft + "px";
-        }
-      }
+}
