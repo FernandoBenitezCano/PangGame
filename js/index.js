d23be608c613fcee63c571d18f6a23daf0268b09
Fernando Benítez-cano Vargas <fbenitez-canov01@iesalbarregas.es>
2023-11-04 14:46:20 +0100

    Player doesnt move
    Has an error i have to fixed it

--- js/index.js
+++ js/index.js
@@ -1,19 +1,15 @@
 import { Player } from "./player.js";
 import { Bullet } from "./bullet.js";
 import { Ball } from "./ball.js";
+
 let gameBoardElement = document.getElementById("gameBoard");
 let startMsg = document.getElementById("startMsg");
-let player = document.createElement("div");
-player.classList.add("player");
-player.style.width=50+"px";
-player.style.height=50+"px";
-let playerSpeed = 15; // player velocity
+let player; // Declarar el jugador fuera de startGame
 
 function keyPressed(event) {
   startMsg.classList.add("hide"); // Oculta el mensaje de inicio
   console.log(event);
-  //Add the listener
-  document.addEventListener("keydown", movementKey);
+
   // Delete the listener
   document.removeEventListener("keydown", keyPressed);
   startGame();
@@ -23,18 +19,26 @@ document.addEventListener("keydown", keyPressed);
 
 // Complete the function startGame
 function startGame() {
-  gameBoardElement.appendChild(player);
-  let bullet= new Bullet(20,20,20,10);
-  let ball= new Ball(20,20,200,200);
+  player = new Player(20, 20, 30, 30);
+  gameBoardElement.appendChild(player.getElement());
+  document.addEventListener("keydown", movementKey);
+  document.addEventListener("keydown", shot);
+  let ball = new Ball(20, 20, 200, 200);
   gameBoardElement.appendChild(ball.getElement());
-  gameBoardElement.appendChild(bullet.getElement());
+}
+
+function shot(event) {
+  if (event.code === "Space") {
+    let bullet = new Bullet(player.x, player.y, 20, 10);
+    gameBoardElement.appendChild(bullet.getElement());
+  }
 }
 
 function movementKey(event) {
   if (event.key === "a" || event.key === "A" || event.key === "ArrowLeft" ) {
-    movePlayer(-playerSpeed);
+    movePlayer(-player.playerSpeed);
   } else if (event.key === "d" || event.key === "D" || event.key === "ArrowRight") {
-    movePlayer(playerSpeed);
+    movePlayer(player.playerSpeed);
   }
 }
 
@@ -46,3 +50,5 @@ function movePlayer(deltaX) {
     player.style.left = newLeft + "px";
   }
 }
+
+
--- js/player.js
+++ js/player.js
@@ -1,20 +1,39 @@
 
 import { GameObject } from "./gameObject.js";
-import { setCustomProperty } from "./updateProperties.js";
 
 export class Player extends GameObject {
+  playerSpeed = 15;
+
   constructor(x, y, width, height) {
     super(x, y, width, height);
     this.setElement();
   }
 
   setElement() {
-    
-    this.element = document.createElement("div");      
-    this.element.classList.toggle("player");
-    this.element.style.width = this.width + "px";
-    this.element.style.height = this.height + "px";
-    document.body.appendChild(this.element);
+
+    this.playerElement = document.createElement("div");
+    this.playerElement.classList.toggle("player");
+    this.playerElement.style.width = this.width + "px";
+    this.playerElement.style.height = this.height + "px";
   }
 
+  getElement() {
+    return this.playerElement;
+  }
+
+  updatePlayer(direction) {
+
+    switch (direction) {
+      case "right":
+        this.x += this.playerSpeed;
+        this.playerElement.style.left = this.x + "px";
+
+      case "left":
+        this.x -= this.playerSpeed; 
+        this.playerElement.style.left = this.x + "px";
+        break;
+    }
+
+
+  }
 }
